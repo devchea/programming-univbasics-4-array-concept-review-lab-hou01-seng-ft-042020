689e40ce57171378658032364223218ca4a8ac03
Richard Chea <rchea817@gmail.com>
2020-03-23 03:20:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
-  array.length.times { |index| array[index] if array[index] == value_to_find}
-  
+  array.length.times { |index| return array[index] if array[index] == value_to_find}
+
 end
 
 def find_max_value(array)
