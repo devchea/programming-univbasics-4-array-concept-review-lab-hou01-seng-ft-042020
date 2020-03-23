75148eea3026b856290fb9985d022d4948822494
Richard Chea <rchea817@gmail.com>
2020-03-23 03:25:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,7 @@
 def find_element_index(array, value_to_find)
   array.length.times { |index|
     if array[index] == value_to_find
-      return index
-    end
-      index += 1
+      return index end index += 1
     }
   nil
 end
